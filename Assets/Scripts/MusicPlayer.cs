6caff1489cb68a51804454efb1ef4110de8832e1
Michael Campos <camposmj89@gmail.com>
2020-11-30 22:24:27 -0800

    Add music player game object
    This includes audiosource and script for storing audio for next scene after newly created Splash scene

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(LoadFirstScene());
+    }
+
+    IEnumerator LoadFirstScene() {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(1);
+    }
+}
