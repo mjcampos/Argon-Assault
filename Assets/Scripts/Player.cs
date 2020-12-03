6e4cf2bcc7a01e1350e0fa12619ed17f7c097795
Michael Campos <camposmj89@gmail.com>
2020-12-03 08:16:29 -0800

    Added Standard assets

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+    [Tooltip("In ms^-1")] [SerializeField] private float _xSpeed = 4f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * _xSpeed * Time.deltaTime;
+        
+        Debug.Log(xOffset);
+    }
+}
